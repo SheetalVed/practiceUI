9bbae4bc2816e64db635f618bb4351b19c516ce6
Sheetal <sheetalved2991@gmail.com>
2024-08-28 14:17:54 +0200

    practiceCode

---
+++ tests/firstTest.spec.js
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('file:///D:/Files/sheetal/mailAndMedia/index.html');
+  await page.getByPlaceholder('Enter a new task').click();
+  await page.getByPlaceholder('Enter a new task').fill('Wakeup!!');
+  await page.getByRole('button', { name: 'Add Task' }).click();
+  await page.getByPlaceholder('Enter a new task').click();
+  await page.getByPlaceholder('Enter a new task').fill('Exercise');
+  await page.getByRole('button', { name: 'Add Task' }).click();
+  await page.getByPlaceholder('Enter a new task').click();
+  await page.getByPlaceholder('Enter a new task').fill('Shower');
+  await page.getByRole('button', { name: 'Add Task' }).click();
+  await page.getByPlaceholder('Enter a new task').click();
+  await page.getByPlaceholder('Enter a new task').fill('Prepare breakfast');
+  await page.getByRole('button', { name: 'Add Task' }).click();
+  await page.close()
+});
